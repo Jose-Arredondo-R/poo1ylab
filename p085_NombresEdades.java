ea38cbddc37eb9021ed1b0291e43aaa68739564f
Jose-Arredondo-R <jose2javier76@gmail.com>
2025-11-13 12:41:01 -0600

    Add files via upload

--- p085_NombresEdades.java
+++ p085_NombresEdades.java
@@ -5,27 +5,23 @@ public class p085_NombresEdades {
         int n, suma = 0, promedio = 0;
         Scanner obj = new Scanner(System.in);
 
-        // Limpiar pantalla (solo funciona en algunas consolas)
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
-        // Preguntar cuántos nombres
         System.out.print("¿Cuántos nombres deseas procesar? ");
         n = obj.nextInt();
 
         String[] nombres = new String[n];
         int[] edades = new int[n];
 
-        // Captura de datos
         for (int i = 0; i < n; i++) {
-            obj.nextLine(); // limpiar buffer
+            obj.nextLine(); 
             System.out.printf("Dame el nombre %d: ", i + 1);
             nombres[i] = obj.nextLine();
             System.out.printf("Dame la edad de %s: ", nombres[i]);
             edades[i] = obj.nextInt();
         }
 
-        // Mostrar resultados
         System.out.println("\nLos nombres capturados son:\n");
         System.out.printf("Nombre\tEdad\n");
 
